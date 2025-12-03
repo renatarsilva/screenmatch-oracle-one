66a7f80cf9f49a88c7cdf919c487ac79ad3be070
Renata <renata.rodriguesdasilva@outlook.com>
2025-12-03 19:00:36 -0300

    feat update

---
+++ src/calculos/Classificavel.java
@@ -0,0 +1,9 @@
+package calculos;
+
+
+// isso é tipo um contrato, que diz que quem usar a classe classsificavel,
+// está se comprometendo em usar o getClassificacao
+
+public interface Classificavel {
+  int getClassificacao();
+}
---
+++ src/modelos/Episodio.java
@@ -0,0 +1,47 @@
+package modelos;
+
+import calculos.Classificavel;
+
+public class Episodio implements Classificavel {
+    private int numero;
+    private String nome;
+    private String serie;
+    private int totalVisualizacoes;
+
+    public int getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return totalVisualizacoes > 100 ? 4 : 2;
+    }
+}
