cf24f4c849c46bda3a24abeaa4aa7bd30652a660
Renata <renata.rodriguesdasilva@outlook.com>
2025-12-13 20:08:39 -0300

    feat: update movies

--- src/principal/PrincipalComBusca.java
+++ src/principal/PrincipalComBusca.java
@@ -5,12 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme: ");
+        var busca = sc.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=6585022c";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=6585022c"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
